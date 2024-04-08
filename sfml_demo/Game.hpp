9994b02e27c84aa74fe06af0ca0e51d4569dbe9c
Liam Nybro <liam.nybro@gmail.com>
2024-04-08 09:47:42 +0200

    Yes sir

---
+++ sfml_demo/Game.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "StateMachine.hpp"
+#include "AssetManger.hpp"
+#include "InputManger.hpp"
+
+namespace Liam
+{
+
+}
--- sfml_demo/InputManger.hpp
+++ sfml_demo/InputManger.hpp
@@ -5,5 +5,14 @@
 
 namespace Liam
 {
+	class InputManager
+	{
+	public:
+		InputManager() {}
+		~InputManager() {}
 
+		bool IsSpriteClicked(sf::Sprite object, sf::Mouse::Button button, sf::RenderWindow& window);
+
+		sf::Vector2i GetMousePosition(sf::RenderWindow &window);
+	};
 }
