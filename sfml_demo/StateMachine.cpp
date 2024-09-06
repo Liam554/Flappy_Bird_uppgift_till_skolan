198a10e3b665b1da5f156dd9dc949f383b18833e
Liam554 <164159619+Liam554@users.noreply.github.com>
2024-09-06 09:22:14 +0200

    Update StateMachine.cpp

--- sfml_demo/StateMachine.cpp
+++ sfml_demo/StateMachine.cpp
@@ -2,6 +2,7 @@
 
 namespace Liam
 {
+	// Adds a new state to the state machine.
 	void StateMachine::AddState(StateRef newState, bool isReplacing)
 	{
 		this->_isAdding = true;
@@ -9,13 +10,13 @@ namespace Liam
 
 		this->_newState = std::move(newState);
 	}
-
+	// Marks the current state for removal.
 	void StateMachine::RemoveState()
 	{
 		this->_isRemoving = true;
 	}
 
-	//Process the states
+	// Processes changes to the state stack (adding/removing states).
 	void StateMachine::ProcessStateChanges()
 	{
 		if (this->_isRemoving && !this->_states.empty())
@@ -29,7 +30,7 @@ namespace Liam
 
 			this->_isRemoving = false;
 		}
-
+	        // Check if a new state needs to be added.
 		if (this->_isAdding)
 		{
 			if (!this->_states.empty())
@@ -51,7 +52,7 @@ namespace Liam
 
 		}
 	}
-	
+	// Returns a reference to the currently active state.
 	StateRef& StateMachine::GetActiveState()
 	{
 		return this->_states.top();
