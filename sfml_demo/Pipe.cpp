20b5416bfda532a532ff3aa3edaaa0761bc6fc71
Liam554 <164159619+Liam554@users.noreply.github.com>
2024-09-06 09:09:15 +0200

    Update Pipe.cpp

--- sfml_demo/Pipe.cpp
+++ sfml_demo/Pipe.cpp
@@ -3,15 +3,16 @@
 
 #include <iostream>
 
-//logiken bakom pipporna i spelet som används som hinder
+
 namespace Liam
 {
+	// Constructor for the Pipe class, initializes game data and sets the land height for pipe spawning.
 	Pipe::Pipe(GameDataRef data) : _data(data)
 	{
 		_landHeight = this->_data->assets.GetTexture("Land").getSize().y;
 		_pipeSpawnYOffset = 0;
 	}
-
+    	// Spawns a bottom pipe at the right side of the window with a random vertical offset.
 	void Pipe::SpawnBottomPipe()
 	{
 		sf::Sprite sprite(this->_data->assets.GetTexture("Pipe Up"));
@@ -20,7 +21,7 @@ namespace Liam
 
 		pipeSprites.push_back(sprite);
 	}
-
+	// Spawns a top pipe at the right side of the window with a random vertical offset.
 	void Pipe::SpawnTopPipe()
 	{
 		sf::Sprite sprite(this->_data->assets.GetTexture("Pipe Down"));
@@ -29,7 +30,7 @@ namespace Liam
 
 		pipeSprites.push_back(sprite);
 	}
-
+	// Spawns an invisible pipe for collision detection purposes (e.g., scoring).
 	void Pipe::SpawnInvisiblePipe()
 	{
 		sf::Sprite sprite(this->_data->assets.GetTexture("Pipe Down"));
@@ -39,7 +40,7 @@ namespace Liam
 
 		pipeSprites.push_back(sprite);
 	}
-
+	// Moves the pipes from right to left across the screen, and removes them if they go off-screen.
 	void Pipe::MovePipes(float dt)
 	{
 		for (int i = 0; i < pipeSprites.size(); i++)
@@ -57,7 +58,7 @@ namespace Liam
 			}
 		}
 	}
-
+	// Draws all the pipe sprites on the screen.
 	void Pipe::DrawPipes()
 	{
 		for (unsigned short int i = 0; i < pipeSprites.size(); i++)
@@ -65,12 +66,12 @@ namespace Liam
 			this->_data->window.draw(pipeSprites.at(i));
 		}
 	}
-
+	// Generates a random vertical offset for the pipes to create variation in their heights.
 	void Pipe::RandomisePipeOffset()
 	{
 		_pipeSpawnYOffset = rand() % (_landHeight + 1);
 	}
-
+	// Returns the vector of pipe sprites for collision detection and other purposes.
 	const std::vector<sf::Sprite>& Pipe::GetSprites() const
 	{
 		return pipeSprites;
