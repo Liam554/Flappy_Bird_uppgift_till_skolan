7231147cc0a4f6f234b0823a8f9ddeb4ec8905d6
Liam554 <164159619+Liam554@users.noreply.github.com>
2024-09-06 08:56:25 +0200

    Update Flash.cpp

--- sfml_demo/Flash.cpp
+++ sfml_demo/Flash.cpp
@@ -2,6 +2,7 @@
 
 namespace Liam
 {
+	// Constructor for the Flash class, initializes the flash effect with a reference to the game data
 	Flash::Flash(GameDataRef data) : _data(data)
 	{
 		_shape = sf::RectangleShape(sf::Vector2f(_data->window.getSize()));
@@ -9,7 +10,7 @@ namespace Liam
 
 		_flashOn = true;
 	}
-
+	// Function to update the flash effect each frame based on delta time (dt)
 	void Flash::Show(float dt)
 	{
 		if (_flashOn)
@@ -24,7 +25,7 @@ namespace Liam
 			
 			_shape.setFillColor(sf::Color(255, 255, 255, alpha));
 		}
-		else
+		else // If the flash is off, decrease the opacity of the rectangle
 		{
 			int alpha = (int)_shape.getFillColor().a - (FLASH_SPEED * dt);
 
@@ -37,7 +38,7 @@ namespace Liam
 			_shape.setFillColor(sf::Color(255, 255, 255, alpha));
 		}
 	}
-
+	// Draw the flash effect on the screen
 	void Flash::Draw()
 	{
 		_data->window.draw(_shape);
