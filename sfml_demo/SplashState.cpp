b9e1fd19e967e6945f2fa5c9a4ada1fd0e48aeae
Liam554 <164159619+Liam554@users.noreply.github.com>
2024-09-06 09:10:55 +0200

    Update SplashState.cpp

--- sfml_demo/SplashState.cpp
+++ sfml_demo/SplashState.cpp
@@ -6,18 +6,19 @@
 
 namespace Liam
 {
+	// Constructor for the SplashState, which takes a reference to the game data.
 	SplashState::SplashState(GameDataRef data) : _data(data)
 	{
 
 	}
-
+    	// Initializes the splash screen by loading the background texture.
 	void SplashState::Init()
 	{
 		_data->assets.LoadTexture("Splash State Background", SPLASH_SCENE_BACKGROUND_FILEPATH);
 
 		_background.setTexture(this->_data->assets.GetTexture("Splash State Background"));
 	}
-
+	// Handles user input for the splash screen state.
 	void SplashState::HandleInput()
 	{
 		sf::Event event;
@@ -30,7 +31,7 @@ namespace Liam
 			}
 		}
 	}
-
+    	// Updates the splash screen. Transitions to the main menu after a certain amount of time.
 	void SplashState::Update(float dt)
 	{
 		if (_clock.getElapsedTime().asSeconds() > SPLASH_STATE_SHOW_TIME)
@@ -38,7 +39,7 @@ namespace Liam
 			_data->machine.AddState(StateRef(new MainMenuState(_data)), true);
 		}
 	}
-
+    	// Draws the splash screen background.
 	void SplashState::Draw(float dt)
 	{
 		_data->window.clear();
